278b8b135c6138c2d622f4e36971e9d824f12a4d
Ruslan <gerarr999@yandex.ru>
2025-02-28 15:54:14 +0300

    add clear allure

--- jenkins.groovy
+++ jenkins.groovy
@@ -5,24 +5,32 @@ pipeline {
     }
 
     stages {
-        stage('Run Tests') {
+        stage('Prepare Environment') {
             steps {
                 script {
                     sh 'chmod +x gradlew'
+                    sh 'rm -rf build/allure-results'
+                    sh 'rm -rf build/reports/allure-report'
+                }
+            }
+        }
 
+        stage('Run Tests') {
+            steps {
+                script {
                     if (params.TEST_TYPE == 'api') {
                         sh './gradlew test -PincludeTags=api'
                     } else if (params.TEST_TYPE == 'ui') {
                         sh './gradlew test -PincludeTags=ui'
                     } else {
-                        // Для выбора 'all' создаем параллельные стадии
                         parallel(
                                 "API Tests": {
                                     sh './gradlew test -PincludeTags=api'
                                 },
                                 "UI Tests": {
                                     sh './gradlew test -PincludeTags=ui'
-                                }
+                                },
+                                failFast: false // Не останавливаем пайплайн, если одна из стадий падает
                         )
                     }
                 }
@@ -32,16 +40,15 @@ pipeline {
         stage('Generate Allure Report') {
             steps {
                 script {
-                    // Генерация Allure отчета с флагом --clean
                     echo "Generating Allure Report"
-                    sh './gradlew allureReport --clean'
+                    sh './gradlew allureReport'
                 }
             }
         }
 
         stage('Allure Report') {
             steps {
-                allure includeProperties: false, jdk: '', results: [[path: 'build/allure-results']]
+                allure includeProperties: false, jdk: '', results: [[path: 'build/allure-results']], allowEmptyResults: true
             }
         }
     }
