46f9904eab65d580676c9688d2814e15b06f445c
Ruslan <gerarr999@yandex.ru>
2025-02-28 15:26:11 +0300

    new script

--- jenkins.groovy
+++ jenkins.groovy
@@ -8,15 +8,36 @@ pipeline {
         stage('Run Tests') {
             steps {
                 script {
+                    // Убедитесь, что gradlew имеет права на выполнение
                     sh 'chmod +x gradlew'
                     if (params.TEST_TYPE == 'api') {
+                        echo "Running API tests"
                         sh './gradlew test -PincludeTags=api'
                     } else if (params.TEST_TYPE == 'ui') {
+                        echo "Running UI tests"
                         sh './gradlew test -PincludeTags=ui'
                     } else {
-                        sh './gradlew test'
+                        echo "Running all tests"
+                        sh './gradlew test'  // Запускает все тесты, если выбран 'all'
                     }
                 }
+            }
+        }
+
+        stage('Generate Allure Report') {
+            steps {
+                script {
+                    // Генерация Allure отчета
+                    echo "Generating Allure Report"
+                    sh './gradlew allureReport'
+                }
+            }
+        }
+
+        stage('Allure Report') {
+            steps {
+                // Публикуем Allure отчет
+                allure includeProperties: false, jdk: '', results: [[path: 'build/allure-results']]
             }
         }
     }
