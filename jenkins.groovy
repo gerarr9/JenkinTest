8d6a693dbabbab49b9258688738cf33ffe9900ae
Ruslan <gerarr999@yandex.ru>
2025-02-28 15:36:22 +0300

    add parallel

--- jenkins.groovy
+++ jenkins.groovy
@@ -8,17 +8,22 @@ pipeline {
         stage('Run Tests') {
             steps {
                 script {
-                    // Убедитесь, что gradlew имеет права на выполнение
                     sh 'chmod +x gradlew'
+
                     if (params.TEST_TYPE == 'api') {
-                        echo "Running API tests"
                         sh './gradlew test -PincludeTags=api'
                     } else if (params.TEST_TYPE == 'ui') {
-                        echo "Running UI tests"
                         sh './gradlew test -PincludeTags=ui'
                     } else {
-                        echo "Running all tests"
-                        sh './gradlew test'  // Запускает все тесты, если выбран 'all'
+                        // Для выбора 'all' создаем параллельные стадии
+                        parallel(
+                                "API Tests": {
+                                    sh './gradlew test -PincludeTags=api'
+                                },
+                                "UI Tests": {
+                                    sh './gradlew test -PincludeTags=ui'
+                                }
+                        )
                     }
                 }
             }
@@ -27,16 +32,15 @@ pipeline {
         stage('Generate Allure Report') {
             steps {
                 script {
-                    // Генерация Allure отчета
+                    // Генерация Allure отчета с флагом --clean
                     echo "Generating Allure Report"
-                    sh './gradlew allureReport'
+                    sh './gradlew allureReport --clean'
                 }
             }
         }
 
         stage('Allure Report') {
             steps {
-                // Публикуем Allure отчет
                 allure includeProperties: false, jdk: '', results: [[path: 'build/allure-results']]
             }
         }
